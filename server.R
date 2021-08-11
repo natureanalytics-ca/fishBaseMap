b2f84329a0ebc4e6832eaa1a21f4fa611c89a852
natureanalytics-ca <bill@natureanalytics.ca>
2021-08-10 20:17:18 -0400

    Added fish images via iNaturalist and created dashbaord layout

---
+++ server.R
@@ -0,0 +1,279 @@
+
+
+#-----------------------------
+#Server
+#-----------------------------
+
+#------------------------------------
+# Loading map data and fishbase data
+#-----------------------------------
+
+##Shape Click Event Leaflet Map in Shiny
+## Zoom in to the country and display corresponding click data when clicked
+
+## Source of shape file
+# http://thematicmapping.org/downloads/world_borders.php
+## Set working directory and Download the shape files 
+
+#download.file("http://thematicmapping.org/downloads/TM_WORLD_BORDERS_SIMPL-0.3.zip" , destfile="TM_WORLD_BORDERS_SIMPL-0.3.zip")
+## Unzip doownloaded file
+#unzip("TM_WORLD_BORDERS_SIMPL-0.3.zip")
+
+
+## Load the shape file to a Spatial Polygon Data Frame (SPDF) using the readOGR() function
+myspdf = readOGR(dsn=getwd(), layer="TM_WORLD_BORDERS_SIMPL-0.3")
+
+#Load fishbase country info
+# fish_master<-country()
+# fish_master<-fish_master %>%
+#   select("country", "Status", "Species")
+# saveRDS(fish_master, file = "fishbase_countries.rds")
+fish_master<-readRDS( file = paste0(getwd(), "/fishbase_countries.rds"))
+
+
+
+server <- function(input, output, session) {
+  
+  #Mapping
+  output$mymap <- renderLeaflet({
+    # Create the map data and add polygons 
+    leaflet(data=myspdf) %>% 
+      addTiles() %>% 
+      setView(lat=10, lng=0, zoom=2.4) %>%
+      addPolygons(fillColor = "green",
+                  highlight = highlightOptions(weight = 5,
+                                               color = "blue",
+                                               fillOpacity = 0.7,
+                                               bringToFront = TRUE),
+                  label = ~NAME,
+                  layerId = ~NAME) # add a layer ID to each shape. This will be used to identify the shape clicked
+    
+  })
+  
+  # Zoom and set the view after click on state shape
+  # input$mymap_shape_click will be NULL when not clicked initially to any shape
+  # input$mymap_shape_click will have the ID, lat and lng corresponding to the shape clicked
+  # Observe to update the view and zoom level when a shape is clicked
+  observe(
+    {  click = input$mymap_shape_click
+    #  subset the spdf object to get the lat, lng and country name of the selected shape (Country in this case)
+    sub = myspdf[myspdf$NAME==input$mymap_shape_click$id, c("LAT", "LON", "NAME")]
+    lat = sub$LAT
+    lng = sub$LON
+    country = sub$NAME
+    if(is.null(click)) ## if nothing is clicked
+      return()
+    else
+      leafletProxy("mymap") %>%
+      setView(lng = lng , lat = lat, zoom = 4) %>%
+      clearMarkers() %>%
+      addMarkers(lng =lng , lat = lat, popup = country)
+    # using lat long from spdf will not change the view on multiple clicks on the same shape
+    
+    }
+  )
+  
+  ## absolute Panel displaying country Name
+  output$map_text <- renderText({
+    sub = myspdf[myspdf$NAME==input$mymap_shape_click$id, c("LAT", "LON", "NAME")]
+    country <- sub$NAME
+    paste("Country:", country)
+  })
+  
+  map_species_list<-reactive({
+    sub = myspdf[myspdf$NAME==input$mymap_shape_click$id, c("LAT", "LON", "NAME")]
+    countryIn = sub$NAME
+    #Manual changes so that country names from map conform to fishbase country names
+    if(length(countryIn) > 0){
+      if(countryIn == "United States") countryIn<-"USA"
+      if(countryIn == "United Kingdom") countryIn <-"UK"
+      if(countryIn == "Marshall islands") countryIn <- "Marshall Is."
+      if(countryIn == "Northern Mariana islands") countryIn <- "North Marianas"
+    }
+    Y<-fish_master %>%
+      filter(
+        country %in% countryIn,
+        Status %in% c("endemic", "native", "introduced", "reintroduced")
+      )
+    Species<-sort(Y$Species)
+    FBname<-species(Species, fields=c("Species", "FBname", "Fresh", "Brack", "Saltwater"))
+    return(FBname)
+  })
+  
+  plot_species_list<-reactive({
+    req(input$fishEnviro)
+    if(input$fishEnviro == "Saltwater") {
+      dtIn<-map_species_list()%>%
+        filter(Saltwater == -1)  %>%
+        mutate(Enviro = rep("Saltwater", NROW(Saltwater))) %>%
+        select("Species", "FBname", "Enviro")
+    }
+    
+    if(input$fishEnviro == "Freshwater") {
+      dtIn<-map_species_list()%>%
+        filter(Fresh == -1) %>%
+        mutate(Enviro = rep("Freshwater", NROW(Fresh))) %>%
+        select("Species", "FBname", "Enviro")
+    }
+    
+    if(input$fishEnviro == "Any environment") {
+      dtIn<-map_species_list() %>%
+        mutate(Enviro = rep("Any", NROW(Saltwater))) %>%
+        select("Species", "FBname", "Enviro")
+    }
+    dtIn
+  })
+  
+  output$species_table <- renderDT({
+    
+    datatable(plot_species_list(),
+              colnames = c("Scientific name", "FishBase common name", "Environment"),
+              rownames = FALSE,
+              selection = "single",
+              options = list(pageLength = 15,
+                             scrollX = TRUE,
+                             columnDefs = list(list(className = 'dt-left', targets = 0:1)))
+    )
+    
+  })
+  
+  species_table_Proxy<-dataTableProxy(session$ns('species_table'))
+  
+  observeEvent(input$species_table_rows_selected,{
+    
+    show_condition <- function(code) {
+      tryCatch(code,
+               error = function(c) NULL,
+               warning = function(c) NULL
+      )
+    }
+    
+    #inaturalist
+    INname<-show_condition(get_inat_obs(taxon_name = plot_species_list()$Species[input$species_table_rows_selected], maxresults = 10))
+    
+    if(NROW(INname) > 0) {
+      
+      INname<-INname %>%
+        filter(license != "") %>%
+        filter(image_url != "") %>%
+        mutate(uselic = str_detect(license, "CC"))
+      
+      if (NROW(INname) > 0) {
+        
+        mxImg<-NROW(INname)
+        item<-carouselEdit(
+          id = "fishCarousel",
+          indicators = TRUE,
+          width = 12,
+          .list =
+            lapply(1:mxImg, function(i) {
+              src = INname$image_url[i]
+              carouselItem(
+                  div(
+                    align ="center",
+                    tags$image(src = src, style="height: 300px; align: center"),
+                    tags$a(href=INname$url[i],
+                           h5("Click for photo credit on iNaturalist.org"),
+                           target="_blank")
+                  )
+              )
+            })
+        )
+      } else {
+        item<-h5("No images found")
+      }
+    } else {
+      item<-h5("No images found")
+    }
+    
+    shinyalert(
+      html = TRUE,
+      closeOnClickOutside = FALSE,
+      size = "s",
+      immediate = TRUE,
+      text = tagList(
+        h3(plot_species_list()$FBname[input$species_table_rows_selected]),
+        h3(plot_species_list()$Species[input$species_table_rows_selected]),
+        item
+        )
+    )
+  })
+  
+  #----------------------------------------------------------------------------------------
+  #Modification of the shinydashboard carousel funtion that prevents auto-advance of slides
+  #----------------------------------------------------------------------------------------
+  
+  carouselEdit <- function (...,
+                            id,
+                            indicators = TRUE,
+                            width = 6,
+                            .list = NULL)
+  {
+    items <- c(list(...), .list)
+    generateCarouselNav <- function(items) {
+      found_active <- FALSE
+      navs <- lapply(seq_along(items), FUN = function(i) {
+        active <- if (found_active) {
+          FALSE
+        } else {
+          sum(grep(x = items[[i]]$attribs$class, pattern = "active")) == 1
+        }
+        if (active && !found_active) found_active <- TRUE
+        shiny::tags$li(
+          `data-target` = paste0("#",id),
+          `data-slide-to` = i - 1,
+          class = if (active) "active"
+        )
+      }
+      )
+      # actives <- dropNulls(lapply(navs, function(nav) {
+      #   nav$attribs$class
+      # }))
+      # if (length(actives) == 0) {
+      navs[[1]]$attribs$class <- "active"
+      items[[1]]$attribs$class <<-
+        paste0(items[[1]]$attribs$class,
+               " active")
+      # }
+      navs
+    }
+    indicatorsTag <-
+      shiny::tags$ol(class = "carousel-indicators",
+                     generateCarouselNav(items))
+    bodyTag <- shiny::tags$div(class = "carousel-inner",
+                               items)
+    controlButtons <- if (indicators) {
+      shiny::tagList(
+        shiny::tags$a(
+          class = "left carousel-control",
+          href = paste0("#", id),
+          `data-slide` = "prev",
+          shiny::tags$span(class = "fa fa-angle-left")
+        ),
+        shiny::tags$a(
+          class = "right carousel-control",
+          href = paste0("#", id),
+          `data-slide` = "next",
+          shiny::tags$span(class = "fa fa-angle-right")
+        )
+      )
+    }
+    else {
+      NULL
+    }
+    carouselTag <-
+      shiny::tags$div(
+        class = "carousel slide",
+        `data-interval` = 'false',
+        `data-ride` = "carousel",
+        `data-pause` = 'hover',
+        id = id
+      )
+    carouselTag <-
+      shiny::tagAppendChildren(carouselTag, indicatorsTag,
+                               bodyTag, controlButtons)
+    shiny::tags$div(class = if (!is.null(width))
+      paste0("col-sm-", width), carouselTag)
+  }
+}
+
---
+++ ui.R
@@ -0,0 +1,218 @@
+
+## Load Required Packages## 
+library(shiny)
+#library(shinydashboard)
+library(fresh)
+library(shinydashboard)
+library(shinydashboardPlus)
+library(leaflet)
+library(rgdal) # R 'Geospatial' Data Abstraction Library. 
+library(shinyWidgets)
+library(rfishbase)
+library(dplyr)
+library(DT)
+library(shinyalert)
+library(shinyjs)
+library(shinycssloaders)
+library(tidyverse)
+library(rinat)
+
+#---------------------------------------------------------------------------------
+# Setup theme (fresh library)
+#
+# Relies on library fresh. 
+# Manual setup required. 
+# Does not take bootstrap buit-in themes
+# NULL will retain default value
+#--------------------------------------------------------------------------------
+
+
+inputTheme <- create_theme(
+  
+  #Change meaning of built in colors
+  adminlte_color(
+    light_blue = "#58C7B1",
+    red = NULL,
+    green = NULL,
+    aqua = NULL,
+    yellow = NULL,
+    blue = NULL,
+    navy = NULL,
+    teal = NULL,
+    olive = NULL,
+    lime = NULL,
+    orange = NULL,
+    fuchsia = NULL,
+    purple = NULL,
+    maroon = NULL,
+    black = NULL,
+    gray_lte = NULL
+  ),
+  
+  #Sidebar
+  adminlte_sidebar(
+    width = NULL,
+    dark_bg = NULL,
+    dark_hover_bg = NULL,
+    dark_color = NULL,
+    dark_hover_color = NULL,
+    dark_submenu_bg = NULL,
+    dark_submenu_color = NULL,
+    dark_submenu_hover_color = NULL,
+    light_bg = NULL,
+    light_hover_bg = NULL,
+    light_color = NULL,
+    light_hover_color = NULL,
+    light_submenu_bg = NULL,
+    light_submenu_color = NULL,
+    light_submenu_hover_color = NULL
+  ),
+  
+  #Global options
+  adminlte_global(
+    content_bg = NULL,
+    box_bg = NULL,
+    info_box_bg = NULL
+  ),
+  
+  #not necessary to write to file
+  output_file = NULL
+)
+
+
+#-----------------------------------------
+#Other layout and styling formatting, HTML
+#-----------------------------------------
+inputStyle<-tags$style(
+  
+  #DT selected row color
+  HTML('table.dataTable tr.selected td, table.dataTable td.selected {
+    background-color: #58C7B1 !important;}'
+  ),
+  
+  #DT scrollX
+  HTML(".dataTables_wrapper { overflow-x: scroll; }" ),
+  
+  #Hyperlink color
+  HTML("a {color: black ;}"),
+  
+  #ShinyWidgets progress bar
+  HTML(".progress-bar{background-color: #58C7B1;}"),
+  
+  #Carousel
+   HTML(".carousel-control {
+    display: none;
+  }"),
+  HTML(".carousel-indicators li { 
+    display: inline-block;
+    width: 12px;
+    height: 12px;
+    margin: -20px;
+    text-indent: 0;
+    cursor: pointer;
+    border: none;
+    border-radius: 50%;
+    background-color: lightgrey;
+  }"),
+  HTML(" .carousel-indicators .active {
+    width: 18px;
+    height: 18px;
+    margin: -20px;
+    background-color: dodgerblue;
+  }"),
+  HTML(".carousel { height:400px;}"),
+
+  #shinyBS popover width
+  HTML(".popover{max-width:50%;}")
+)
+
+
+#----------------------------------------
+#Footer - for NA copyright, terms of use
+#----------------------------------------
+footer = dashboardFooter(
+  right = div(fluidRow("Copyright Nature Analytics"),
+              fluidRow(tags$a(href="https://natureanalytics.ca/terms","Terms of use", 
+                              target="_blank")
+              )
+  ),
+  left = tags$a(href="https://natureanalytics.ca", 
+                tags$image(src="black_logo_transparent_background.png", 
+                           height=60, 
+                           width=128
+                ),  
+                target="_blank")
+)
+
+#----------------------------
+# UI
+#----------------------------
+
+header <- dashboardHeader(
+  title = div(align="left", "Fish geo")
+)
+
+sidebar <- dashboardSidebar(
+  #minified = FALSE,
+  sidebarMenu(
+    menuItem(
+      tabName = "mappingTool",
+      text = "World map"
+    )
+  )
+)
+
+
+body<-dashboardBody(
+  
+  useShinyjs(),
+  useShinyalert(),
+  
+  #----------------------
+  #Read styling items
+  #----------------------
+  use_theme(inputTheme),
+  inputStyle,
+
+  tabItems(
+    
+    #---------------------------
+    #Boxes
+    #---------------------------
+    
+    tabItem(
+      tabName = "mappingTool",
+      br(),
+      h4("Select a country to generate its corresponding fish species list."),
+      withSpinner(leafletOutput("mymap")),
+      br(),
+      br(),
+      radioButtons(
+        inputId = "fishEnviro",
+        label = "Filter by environment",
+        choices = c("Saltwater", "Freshwater", "Any environment")
+      ),
+      br(),
+      h4("Select a row from the table to view images of the selected species."),
+      br(),
+      withSpinner(DTOutput("species_table")),
+      # Absolute panel will house the user input widgets
+      # Div tag is used to add styling to absolute panel
+      absolutePanel(top = 130, right = 20, fixed = FALSE,
+                    tags$div(style = "opacity: 0.70; background: #FFFFFF; padding: 8px; ",
+                             helpText("Welcome to the World map!"),
+                             textOutput("map_text") # display the country name in absolute panel when shape is clicked
+                    )
+      )
+    )
+  )
+)
+
+
+shinydashboardPlus::dashboardPage(
+  skin = "black",
+  header = header,
+  sidebar = sidebar,
+  body = body,
+  footer = footer
+)
